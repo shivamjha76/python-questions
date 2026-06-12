4bb5f9aee81ac0440e0ae92f9c88835ace8f961a
shivam jha <shivamjha7622@gmail.com>
2026-06-12 21:57:14 +0530

    Add program to calculate area of a triangle

---
+++ Bigineers/Mathematics/area_of_triangle.py
@@ -0,0 +1,8 @@
+# write a python program to find the area of the triangle.
+
+base = float(input("Enter the value of base: "))
+height = float(input("Enter the value of height: "))
+
+area = 1/2 * (base * height)
+
+print(f"area of the trangle = {area}")
