2daf021940c6ceca96fedd78d0f986079035accd
shivam jha <shivamjha7622@gmail.com>
2026-06-12 22:20:01 +0530

    Implement two methods to swap two variables
    Added a second method for swapping two variables using a temporary variable.

--- Bigineers/swap_two_variables.py
+++ Bigineers/swap_two_variables.py
@@ -1,3 +1,6 @@
+# write a program to swap the values of two variables
+
+# Method 1
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
 
@@ -5,6 +8,20 @@ print("Original values")
 print(f"a = {a} and b = {b}")
 
 a, b = b, a
+
+print("After swap")
+print(f"a = {a} and b = {b}")
+
+# Method 2
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+print("Original values")
+print(f"a = {a} and b = {b}")
+
+temp = a
+a = b
+b = temp
 
 print("After swap")
 print(f"a = {a} and b = {b}")
