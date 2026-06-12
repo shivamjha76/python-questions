9863d6b48b7513621c587cb71ba4fae275d8848c
shivam jha <shivamjha7622@gmail.com>
2026-06-12 21:53:09 +0530

    Add basic arithmetic operations in Python

---
+++ Bigineers/Mathematical_operation_of_two_number.py
@@ -0,0 +1,9 @@
+# write a python program to add, sub, multiply, divide two numbers.
+
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
