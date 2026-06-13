cba94e7b86fdba0b12c10eb6e5c9f61cf5391087
shivam jha <shivamjha7622@gmail.com>
2026-06-13 22:11:48 +0530

    Add km_to_miles.py for distance conversion
    This program takes a distance in kilometers as input and converts it to miles using the conversion factor.

---
+++ Bigineers/Mathematics/km_to_miles.py
@@ -0,0 +1,6 @@
+# write a python program to convert kilometers into miles.
+
+distance = float(input("Enter distance in kilometers: "))
+miles = distance * 0.621371
+
+print(f"Distance in miles = {miles}")
