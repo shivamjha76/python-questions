7e63d42e2f79d6ec0b9521443f360d4558e40a34
shivam jha <shivamjha7622@gmail.com>
2026-06-13 22:22:12 +0530

    Create celsius_to_fahrenheit.py for temperature conversion
    Add a program to convert Celsius to Fahrenheit.

---
+++ Bigineers/Mathematics/celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+#y write a pyhton program to convert celsius to fahrenheit.
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = celsius * (9/5) + 32
+
+print(f"Temperature in Fahrenheit = {fahrenheit:.2f}")
